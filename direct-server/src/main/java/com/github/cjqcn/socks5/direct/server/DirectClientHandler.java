27d6960f563d38e2ca924d8ef6536827f11ca84f
cjqCn <20170402cjq@gmail.com>
2018-08-15 20:16:45 +0800

    fix

--- direct-server/src/main/java/com/github/cjqcn/socks5/direct/server/DirectClientHandler.java
+++ direct-server/src/main/java/com/github/cjqcn/socks5/direct/server/DirectClientHandler.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package com.github.cjqcn.socks5.direct.server.socksproxy;
+package com.github.cjqcn.socks5.direct.server;
 
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
